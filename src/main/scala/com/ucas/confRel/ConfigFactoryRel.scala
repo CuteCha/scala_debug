809bad96351777055638ec17048a886ae224c8b0
caoxiaoqiang <checao@tencent.com>
2022-04-14 11:30:13 +0800

    bit operation

--- src/main/scala/com/ucas/confRel/ConfigFactoryRel.scala
+++ src/main/scala/com/ucas/confRel/ConfigFactoryRel.scala
@@ -64,9 +64,65 @@ object ConfigFactoryRel {
     MurHash32.stringHash(s, seed).abs.toString
   }
 
+  def test03(): Unit = {
+    var a = 60
+    /* 60 = 0011 1100 */
+    var b = 13
+    /* 13 = 0000 1101 */
+    var c = 0
+
+    c = a & b /* 12 = 0000 1100 */
+    println("a & b = " + c);
+
+    c = a | b /* 61 = 0011 1101 */
+    println("a | b = " + c)
+
+    c = a ^ b /* 49 = 0011 0001 */
+    println("a ^ b = " + c)
+
+    c = ~a /* -61 = 1100 0011 */
+    println("~a = " + c)
+
+    c = a << 2 /* 240 = 1111 0000 */
+    println("a << 2 = " + c);
+
+    c = a >> 2 /* 15 = 1111 */
+    println("a >> 2  = " + c);
+
+    c = a >>> 2 /* 15 = 0000 1111 */
+    println("a >>> 2 = " + c)
+    println(s"[${(-6).toBinaryString}] -6 >>> 2 = [${((-6) >>> 2).toBinaryString}] ${(-6) >>> 2}")
+    println(s"[${6.toBinaryString}] 6 >>> 2 = [${(6 >>> 2).toBinaryString}] ${6 >>> 2}")
+
+    println(s"-60=${(-60).toBinaryString}")
+    println(s"60=${60.toBinaryString}")
+
+    println(s"${Integer.parseInt("11", 2)}=11")
+    println(s"${Integer.parseInt("111100", 2)}=111100")
+
+    bitCal(-6)
+    bitCal(6)
+
+
+  }
+
+  def bitCal(i: Int): Unit = {
+    val bitStr = i.toBinaryString
+    val l = bitStr.length
+    val r = i >> 2
+    val rBitStr = r.toBinaryString
+    val rL = rBitStr.length
+    val ru = i >>> 2
+    val ruBitStr = ru.toBinaryString
+    val ruL = ruBitStr.length
+
+    println(s"[$bitStr](len=$l) $i >>> 2 = [$ruBitStr](len=$ruL) $ru")
+    println(s"[$bitStr](len=$l) $i >> 2 = [$rBitStr](len=$rL) $r")
+  }
+
 
   def main(args: Array[String]): Unit = {
-    test02()
+    test03()
   }
 
 }
